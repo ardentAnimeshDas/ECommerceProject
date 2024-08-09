ac5123508b6b310f5dd7c5ac6d62db5bc6f0e81f
Animesh Das <animesdas.2013@gmail.com>
2024-08-09 21:31:23 +0530

    Project Started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <Container/>
+    </div>
+  );
+}
+
+export default App;
