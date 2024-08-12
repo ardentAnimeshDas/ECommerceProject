ec0a72ea75f65b61c84710745d69f760b8817074
animesdas2013 <animesdas.2013@gmail.com>
2024-08-12 12:43:18 +0530

    Added the carousel content

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import CarouselMenu from './Homepage/CarouselMenu';
 
 const Container = () => {
     return (
         <>
-            <div className="container">
-                
+            <div className="container-fluid mt-5">
+                <div className="row">
+                    <CarouselMenu/>
+                </div>
             </div>
         </>
     )
---
+++ src/components/Homepage/CarouselMenu.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import '../../css/Carousel.css';
+
+const CarouselMenu = () => {
+  const images = [
+    'smartwatch.png',
+    'ps4.png',
+    'iphone.png'
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    console.log("Animesh")
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  return (
+    <>
+      <div className="custom-carousel-container col">
+        <button className="carousel-button" onClick={prevSlide}>Prev</button>
+        <div className="carousel-slide">
+          <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
+        </div>
+        <button className="carousel-button" onClick={nextSlide}>Next</button>
+      </div>
+    </>
+  );
+};
+
+export default CarouselMenu;
