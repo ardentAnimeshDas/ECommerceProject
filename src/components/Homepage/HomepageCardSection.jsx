20abf6042599e2885856c897b4d4df2a7d1b96d4
Swapnadeep <reconnectingmyself@proton.me>
2024-08-13 08:30:14 +0530

    kart css added

--- src/components/Homepage/HomepageCardSection.jsx
+++ src/components/Homepage/HomepageCardSection.jsx
@@ -1,42 +1,63 @@
 import React from 'react';
+import '../../css/HomepageCardSection.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const HomepageCardSection = () => {
     return (
         <>
-            <div className='col-12 col-3 card'>
-                <img className="card-img-top" src="" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+            <div class="row container-fluid">
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/product-item1.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
                     </div>
-            </div>
+                </div>
 
-            <div className='col-12 col-3 card'>
-                <img className="card-img-top" src="" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/product-item2.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
                     </div>
-            </div>
+                </div>
 
-            <div className='col-12 col-3 card'>
-                <img className="card-img-top" src="" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/product-item3.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
                     </div>
-            </div>
+                </div>
 
-            <div className='col-12 col-3 card'>
-                <img className="card-img-top" src="" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/product-item4.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
                     </div>
+                </div>
             </div>
         </>
     );
