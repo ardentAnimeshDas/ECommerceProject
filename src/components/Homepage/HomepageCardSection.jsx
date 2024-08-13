c49da4865a508b36873cd3eaac7b74d918e63d6c
Swapnadeep <reconnectingmyself@proton.me>
2024-08-13 11:20:04 +0530

    iphone

--- src/components/Homepage/HomepageCardSection.jsx
+++ src/components/Homepage/HomepageCardSection.jsx
@@ -10,7 +10,7 @@ const HomepageCardSection = () => {
                 <div class="col-sm-6 col-md-3">
                     <div class="Card">
                         <div class="card-body">
-                        <img src='/Images/home_product/product-item1.jpg' alt='img'/>
+                        <img src='/Images/home_product/iphone/product-item1.jpg' alt='img'/>
                         <h2 className='item-name'>Iphone 11</h2>
                         <span className='item-description'>M.R.P.: ₹44,999.00</span>
                         <div class="hide">
@@ -23,7 +23,7 @@ const HomepageCardSection = () => {
                 <div class="col-sm-6 col-md-3">
                     <div class="Card">
                         <div class="card-body">
-                        <img src='/Images/home_product/product-item2.jpg' alt='img'/>
+                        <img src='/Images/home_product/iphone/product-item2.jpg' alt='img'/>
                         <h2 className='item-name'>Iphone 11</h2>
                         <span className='item-description'>M.R.P.: ₹44,999.00</span>
                         <div class="hide">
@@ -36,7 +36,7 @@ const HomepageCardSection = () => {
                 <div class="col-sm-6 col-md-3">
                     <div class="Card">
                         <div class="card-body">
-                        <img src='/Images/home_product/product-item3.jpg' alt='img'/>
+                        <img src='/Images/home_product/iphone/product-item3.jpg' alt='img'/>
                         <h2 className='item-name'>Iphone 11</h2>
                         <span className='item-description'>M.R.P.: ₹44,999.00</span>
                         <div class="hide">
@@ -49,7 +49,62 @@ const HomepageCardSection = () => {
                 <div class="col-sm-6 col-md-3">
                     <div class="Card">
                         <div class="card-body">
-                        <img src='/Images/home_product/product-item4.jpg' alt='img'/>
+                        <img src='/Images/home_product/iphone/product-item4.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row container-fluid">
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/iphone/product-item1.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/iphone/product-item2.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/iphone/product-item3.jpg' alt='img'/>
+                        <h2 className='item-name'>Iphone 11</h2>
+                        <span className='item-description'>M.R.P.: ₹44,999.00</span>
+                        <div class="hide">
+                            <button class="kart"><FontAwesomeIcon icon={faShoppingCart}/>Add to Cart</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3">
+                    <div class="Card">
+                        <div class="card-body">
+                        <img src='/Images/home_product/iphone/product-item4.jpg' alt='img'/>
                         <h2 className='item-name'>Iphone 11</h2>
                         <span className='item-description'>M.R.P.: ₹44,999.00</span>
                         <div class="hide">
